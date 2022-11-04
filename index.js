fac7ba1d5cb9570c01749104636d24216b3302a0
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-04 14:48:44 +0000

    add failed form test index.test.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// function createTaskHtml() {
+//   const newHtml = `<p></p>`
+//   return newHtml;
+// }
 function createTaskHtml() {
-  const newHtml = `<p></p>`
-  return newHtml;
+  const newFormHtml = `<form action="">
+  <fieldset>
+    <legend>
+      <label for="create-task">Create your to-do task</label>
+      <input 
+      type="text"
+      id="create-task"
+      name="create-task"
+      required
+      >
+    </legend>
+  </fieldset>
+</form>`
+  return newFormHtml;
 }
 
