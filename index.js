1e13677c21a1c90261c7c429784e6be2d976ce2e
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-04 13:48:44 +0000

    add test index.test.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
+function createTaskHtml() {
+  const newHtml = `<p></p>`
+  return newHtml;
+}
 
--- index.test.js
+++ index.test.js
@@ -1,3 +1,8 @@
-
+// Create form html tests
+test('fn should return new html', () => {
+  const actual = createTaskHtml();
+  const expected = `<p></p>`;
+  equal(actual, expected);
+})
 
   
