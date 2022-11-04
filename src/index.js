28c38ac7b228691fd74537c26765eed70e2d36ea
Natalia Rusu <bonapartik@gmail.com>
2022-11-04 20:24:12 +0000

    add close button
    Relates #4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const backdrop = document.getElementById('backdrop');
 const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
 const addToDoBtn = document.querySelector('#add-todo');
 
 const USER_NAME = 'user-name';
@@ -9,18 +10,24 @@ const removeBackdrop = () => {
   backdrop.classList.remove('visible');
 };
 
+const resetModal = () => {
+  modal.querySelector('.form-title').innerHTML = '';
+  modal.removeChild(modal.querySelector('.form-wrapper'));
+  removeBackdrop();
+};
+
 const addTaskHandler = (ev) => {
   ev.preventDefault();
   console.log('form submitted handler');
   //implement logic to consume data here
-  modal.innerHTML = '';
-  removeBackdrop();
+  resetModal();
 };
 
 const addModalContent = (type) => {
   if (type === ADD_TO_DO) {
-    const titleEl = document.createElement('h3');
+    const titleEl = modal.querySelector('.form-title');
     const formWrapper = document.createElement('div');
+    formWrapper.classList.add('form-wrapper');
     titleEl.innerHTML = 'title';
     //call create form Gareth with different input type to add-to-do use input type text
     formWrapper.innerHTML = createForm(
@@ -28,8 +35,10 @@ const addModalContent = (type) => {
       'Enter task',
       'create-task'
     );
-    modal.append(titleEl, formWrapper);
-    const formBtn = document.querySelector('#add-task-form button');
+    modal.append(formWrapper);
+    const formBtn = document.querySelector(
+      '#add-task-form button[type="submit"]'
+    );
     formBtn.addEventListener('click', addTaskHandler);
   } else if (type === USER_NAME) {
     //call add name from Gareth with different input type to handle user name and store it
@@ -66,7 +75,7 @@ function createForm(formId, labelText, inputId) {
       >
     <button type="submit">Submit</button>
 </form>`;
-
 }
 
 addToDoBtn.addEventListener('click', addToDoHandler);
+modalCloseBtn.addEventListener('click', resetModal)
