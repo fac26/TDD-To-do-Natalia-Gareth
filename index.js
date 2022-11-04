1745b46a1ee03dcbc54fec9602205b9ab7fdeb38
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-04 13:40:51 +0000

    remove map function test code

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-function map(array) {
-  return array;
-}
+
 
--- index.test.js
+++ index.test.js
@@ -1,6 +1,3 @@
-test("map() should return an array with the same number of elements", () => {
-    const result = map([1], () => {});
-    equal(result.length, 1);
-  });
+
 
   
