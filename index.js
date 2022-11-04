0f315a4d600583b9fcae31e0646f5249a71ddbcf
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-04 16:43:53 +0000

    add passed populate div with form test index.test.js

--- index.js
+++ index.js
@@ -2,20 +2,42 @@
 //   const newHtml = `<p></p>`
 //   return newHtml;
 // }
+
+// function createTaskHtml() {
+//   const newFormHtml = `<form action="">
+//   <fieldset>
+//     <legend>
+//       <label for="create-task">Create your to-do task</label>
+//       <input 
+//       type="text"
+//       id="create-task"
+//       name="create-task"
+//       required
+//       >
+//     </legend>
+//   </fieldset>
+// </form>`
+//   return newFormHtml;
+// }
+
+const inputFormDiv = document.querySelector("#to-do-task-input-form-div");
+
 function createTaskHtml() {
   const newFormHtml = `<form action="">
   <fieldset>
-    <legend>
-      <label for="create-task">Create your to-do task</label>
+    <legend>Create your to-do task</legend>
+      <label for="create-task">Enter task</label>
       <input 
       type="text"
       id="create-task"
       name="create-task"
       required
       >
-    </legend>
   </fieldset>
 </form>`
-  return newFormHtml;
+
+  inputFormDiv.innerHTML = newFormHtml;
+
+  return inputFormDiv.innerHTML;
 }
 
--- index.test.js
+++ index.test.js
@@ -1,7 +1,32 @@
 // Create form html tests
-test('fn should return new html', () => {
+
+// test('fn should return new html', () => {
+//   const actual = createTaskHtml();
+//   const expected = `<p></p>`;
+//   equal(actual, expected);
+// })
+
+// test('fn should return new html form', () => {
+//   const actual = createTaskHtml();
+//   const expected = `<form action="">
+//   <fieldset>
+//     <legend>
+//       <label for="create-task">Create your to-do task</label>
+//       <input 
+//       type="text"
+//       id="create-task"
+//       name="create-task"
+//       required
+//       >
+//     </legend>
+//   </fieldset>
+// </form>`;
+//   equal(actual, expected);
+// })
+
+test('fn should populate div with new html form', () => {
   const actual = createTaskHtml();
-  const expected = `<p></p>`;
+  const expected = inputFormDiv.innerHTML;
   equal(actual, expected);
 })
 
