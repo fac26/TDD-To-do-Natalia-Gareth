46365c281f2e9bc35e69867c83dc0368b83f0b9f
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-04 17:16:15 +0000

    remove tested iterations of createTaskHtml function index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-// function createTaskHtml() {
-//   const newHtml = `<p></p>`
-//   return newHtml;
-// }
-
-// function createTaskHtml() {
-//   const newFormHtml = `<form action="">
-//   <fieldset>
-//     <legend>
-//       <label for="create-task">Create your to-do task</label>
-//       <input 
-//       type="text"
-//       id="create-task"
-//       name="create-task"
-//       required
-//       >
-//     </legend>
-//   </fieldset>
-// </form>`
-//   return newFormHtml;
-// }
+// Selected elements
 
 const inputFormDiv = document.querySelector("#to-do-task-input-form-div");
 
+// This function populates the inputFormDiv with the html form
 function createTaskHtml() {
   const newFormHtml = `<form action="">
   <fieldset>
