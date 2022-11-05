3fb91ded1bf92929b13edd13b662d754722185bc
Natalia Rusu <bonapartik@gmail.com>
2022-11-05 13:07:22 +0000

    x

--- src/index.test.js
+++ src/index.test.js
@@ -14,14 +14,23 @@ test('when button Add-to-do was clicked, modal shows form with id=add-task-form'
     const expected= true;
     equal(actual, expected)
 });
-test('click button submit should remove form from modal',()=>{
-    const button = document.querySelector('#add-task-form button[type="submit"]');
-    button.click();
+test('click submit should remove form from modal',()=>{
     const form = document.getElementById('add-task-form');
+    const button = form.querySelector('button[type="submit"]');
+    form.querySelector('input').value='xxx';
+    button.click();//we can't listen to submit event here?    
     const actual=document.body.contains(form);
     const expected=false
     equal(actual, expected);
+    ToDoList.length=0;
+});
 
+test('addToDoItem("handle event listener") should add item to ToDoList array', ()=>{
+    addToDoItem('handle event listener');
+    const actual=ToDoList[0].title;
+    const expected='handle event listener';
+    equal(actual, expected); 
+    ToDoList.length=0;
 });
 
 /***************************************************************** */
@@ -57,6 +66,3 @@ test('createForm() should return form', () => {
   const expected = true;
   equal(actual, expected);
 })
-
-
-  
